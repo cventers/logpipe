2b1900c53e7ee3cde70f431dd046c8757ece16f9
Chase Venters <chase.venters@chaseventers.com>
2013-10-24 14:46:58 -0500

    Add project files
    Signed-off-by: Chase Venters <chase.venters@chaseventers.com>

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ logpipe
 =======
 
 STDIN to UNIX-domain socket log writer with prefix and reconnect support, written in Go
+
+My use-case for logpipe is to serve as a destination for Apache logs:
+
+ErrorLog "|/usr/local/bin/logpipe -socket /tmp/syslog-ng-apache-in.sock"
+
+You could do something similar with socat or a similar utility, but I want
+an easy way to add prefixes to log lines before they reach syslog-ng, and
+I don't want to redefine Apache's log formats.
+
+Plus I wanted to write some Go.
---
+++ logpipe.go
@@ -0,0 +1,110 @@
+/* ======================================================================== */
+/* logpipe.go - STDIN to UNIX-domain socket log writer                      */
+/* Written by Chase Venters <chase.venters@gmail.com>, Public Domain        */
+/* ======================================================================== */
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+// Define flags
+var f_socketpath = flag.String("socket", "", "Path to the log socket")
+var f_prefix = flag.String("prefix", "", "Prefix to add to lines")
+var f_socket_type = flag.String("socket-type", "stream",
+	"Type of UNIX-domain socket connection (stream/dgram)")
+var f_reconnect_time = flag.Int("reconnect-time", 1,
+	"Time to wait (in seconds) before reconnect")
+
+// Initialize
+func main() {
+	flag.Parse()
+
+	if *f_socketpath == "" {
+		log.Fatal("-socket is a required argument")
+	}
+
+	var socktype string
+	if *f_socket_type == "stream" {
+		socktype = "unix"
+	} else if *f_socket_type == "dgram" {
+		socktype = "unixgram"
+	} else {
+		log.Fatal("-socket-type must be stream or dgram")
+	}
+
+	for {
+		run(*f_socketpath, socktype, *f_prefix)
+		if *f_reconnect_time > 0 {
+			log.Printf(
+				"Pausing %d seconds until reconnect", *f_reconnect_time)
+			time.Sleep(time.Duration(*f_reconnect_time) * time.Second)
+		} else {
+			// No reconnect: Bail with error
+			os.Exit(1)
+		}
+	}
+}
+
+var nr_conns = 0
+var strout string
+
+func run(socketpath string, sockettype string, prefix string) {
+	// Connect to UNIX-domain socket
+	conn, err := net.Dial(sockettype, socketpath)
+	if err != nil {
+		log.Print("Connection failed: ", err.Error())
+		if nr_conns == 0 {
+			// No successful connections have happened, so we haven't
+			// read anything from STDIN and we can safely exit now.
+			os.Exit(1)
+		} else {
+			return
+		}
+	}
+	nr_conns++
+
+	scanner := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(conn)
+
+	// Keep writing data
+	for {
+		// Build output string
+		if strout != "" {
+			// Write string to output buffer
+			_, err = writer.WriteString(strout)
+			if err != nil {
+				log.Print("Write failed: ", err.Error())
+				return
+			}
+			err = writer.Flush()
+			if err != nil {
+				log.Print("Flush failed: ", err.Error())
+				return
+			}
+		}
+
+		// If we didn't get any more data, exit the loop
+		if !scanner.Scan() {
+			break
+		}
+
+		// Queue data for writing
+		strout = prefix + scanner.Text() + "\n"
+	}
+
+	// Reader errors result in immediate exit
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// On EOF, we just bail...
+	os.Exit(0)
+}
+
